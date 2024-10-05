d9403bf929c7f62fff5389748a141ca58fd6aacc
SharafEddine1 <dahane.charafeddine@gmail.com>
2024-10-06 00:32:17 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+
+document.getElementById('exploreButton').addEventListener('click', function() {
+    alert('استكشاف المزيد من المنتجات قريبا!');
+});
+
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('شكرًا لتواصلك معنا!');
+});
